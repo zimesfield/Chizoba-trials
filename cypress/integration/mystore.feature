c0a49160196b72e8d8cb4edda7527605af387acb
Chizoba Ekebere <ekeberechizoba@gmail.com>
2022-06-07 17:18:29 +0100

    second commit

--- cypress/integration/mystore.feature
+++ cypress/integration/mystore.feature
@@ -1,10 +1,10 @@
 Feature: title
     Scenario: shop and checkout
-     Given I visit the website
-     Then I should see the homepage
-      When  I add one item to cart
-      And I add another item to cart
-      And I proceed to check out
-      Then I should see the items in my cart with the final figure
+        Given I visit the website
+        Then I should see the homepage
+        When I add one item to cart
+        And I add another item to cart
+        And I proceed to check out
+        Then I should see the items in my cart with two items in the cart
      
  
--- cypress/support/step_definitions/mystore.steps.ts
+++ cypress/support/step_definitions/mystore.steps.ts
@@ -10,19 +10,23 @@ import {Given, When,Then} from "cypress-cucumber-preprocessor/steps"
 
 
     When("I add one item to cart",()=>{
-        cy.get("a[data-id-product='3']").click({force:true,multiple:true})
-       /* cy.get('span[title="Continue shopping"]')
-           .click()
-        cy.get("#homefeatured")
-         .click()
-        cy.get("a[href='http://automationpractice.com/index.php?controller=cart&add=1&id_product=6&token=e817bb0705dd58da8db074c69f729fd8']")
-          .click({multiple:true,force:true})*/
+        cy.get("a[href='http://automationpractice.com/index.php?id_product=4&controller=product'][title='Printed Dress']").click({multiple:true,force:true})
+        cy.get("#add_to_cart").click()
+        cy.get("#columns").click()
+        cy.get('a[href="http://automationpractice.com/index.php?id_category=8&controller=category"]').click({multiple:true,force:true})
     })
+      
+     When("I add another item to cart",()=>{
+        cy.get("a[href='http://automationpractice.com/index.php?id_product=5&controller=product'][title='Printed Summer Dress']").click({multiple:true,force:true})
+        cy.get("#add_to_cart").click()
+     })
        
-        When("I add another item to cart",()=>{
+    When("I proceed to check out",()=>{ 
             cy.get('a[href="http://automationpractice.com/index.php?controller=order"]')
-                .click({multiple:true})
-            
-         
+                .click({multiple:true})  
+    })
+
+    Then("I should see the items in my cart with two items in the cart",()=>{
+            cy.get("")
     })
     
